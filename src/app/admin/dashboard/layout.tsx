adc1f9cf82c083a9ed67d9d135c5f879c0662d9a
parinTNK <parin.tnk@gmail.com>
2025-05-16 18:53:52 +0700

    fix admin layout
    - fix  sidebar show at admin login page

---
+++ src/app/admin/dashboard/layout.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Sidebar from "../components/Sidebar";
+import { CoursesProvider } from "../context/CoursesContext";
+import { Toaster } from "sonner";
+
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <CoursesProvider>
+      <div className="flex">
+        <div className='fixed'>
+        <Sidebar />
+        </div>
+        <main className="flex-1 bg-gray-50 min-h-screen ml-[240px]">
+          {children}
+        </main>
+      </div>
+    </CoursesProvider>
+  );
+}
